6f0cbccce93fff6469ae14869cdec20e7879a1e3
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-01-12 14:45:51 +0100

    use datahandler

--- Classes/Controller/AddRedirectsController.php
+++ Classes/Controller/AddRedirectsController.php
@@ -11,17 +11,17 @@ use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Http\HtmlResponse;
 use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
-
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 class AddRedirectsController  extends BackendModuleActionController
 {
-    /**
-     * @var ServerRequestInterface
-     */
-    protected $request;
-
+    const NUMBER_OF_ROW = 8;
+    const LANG_FILE = 'LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:';
     /**
      * ModuleTemplate object
      *
@@ -29,15 +29,35 @@ class AddRedirectsController  extends BackendModuleActionController
      */
     protected $moduleTemplate;
 
+    /**
+     * @var LocalizationUtility
+     */
+    protected $localizationUtility;
+
     /*
      * @var RedirectRepository
      */
     private $redirectRepository;
 
+    protected $separatedChars = [
+        'tab' => "\t",
+        'pipe' => '|',
+        'semicolon' => ';',
+        'colon' => ':',
+        'comma' => ',',
+    ];
+
+    protected $selectedSeparatedChar = '';
+
     protected  $duplicatedSourcePath = '';
 
-    public function __construct(ModuleTemplate $moduleTemplate = null, RedirectRepository $redirectRepository = null)
+    public function __construct(
+        ModuleTemplate $moduleTemplate = null,
+        RedirectRepository $redirectRepository = null,
+        LocalizationUtility $localizationUtility = null
+    )
     {
+        $this->localizationUtility = $localizationUtility ?? GeneralUtility::makeInstance(LocalizationUtility::class);
         $this->moduleTemplate = $moduleTemplate ?? GeneralUtility::makeInstance(ModuleTemplate::class);
         $this->redirectRepository = $redirectRepository ?: GeneralUtility::makeInstance(RedirectRepository::class);
     }
@@ -59,76 +79,73 @@ class AddRedirectsController  extends BackendModuleActionController
         if(is_null($request)){
             return null;
         }
-        $this->request = $request;
-        // rendering after form submit
-        /** @var StandaloneView $view */
+
         $view = GeneralUtility::makeInstance(StandaloneView::class);
         $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName(
             'EXT:qc_redirects/Resources/Private/Templates/Import.html'
         ));
-        if(!is_null($request)){
-            $redirectsList = $request->getParsedBody()['redirectsList'];
-            $character = $this->request->getParsedBody()['indentationCharacter'];
-        }
 
+        $redirectsList = $request->getParsedBody()['redirectsList'];
+        $character = $request->getParsedBody()['indentationCharacter'];
+        $this->selectedSeparatedChar = $request->getParsedBody()['separationCharacter'];
         if(!is_null($redirectsList)){
             // convert data to array
             $redirectsListArray = explode("\r\n", $redirectsList);
-            $created = $this->createRedirectsList($redirectsListArray);
+            $created = $this->importRedirects($redirectsListArray);
             // alert message
-            $message = '';
-            if($created == true){
-                $message = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessage::class,
-                    $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:import_success_body'),
-                    $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:success'),
-                    \TYPO3\CMS\Core\Messaging\FlashMessage::OK,
-                );
-                $flashMessageService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessageService::class);
-                $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
-                $messageQueue->addMessage($message);
-            }
-            else {
-                if($this->duplicatedSourcePath == ''){
-                    $message = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessage::class,
-                        $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:import_error_syntax'),
-                        $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:error'),
-                        \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR,
-                    );
-                }
-                else{
-                    $message = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessage::class,
-                        $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:import_error_duplicated')
-                        . ' "'. $this->duplicatedSourcePath .'"'. $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:is_duplicated') ,
-                        $this->getLanguageService()->sL('LLL:EXT:qc_redirects/Resources/Private/Language/locallang.xlf:error'), // [optional] the header
-                        \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR,
-                    );
-                }
-
-                $flashMessageService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessageService::class);
-                $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
-                $messageQueue->addMessage($message);
-            }
+            $this->generateAlertMessage($created);
         }
         $view->assign('character', $character);
+        $view->assign('separatedChars', $this->separatedChars);
         $this->moduleTemplate->setContent($view->render());
         return new HtmlResponse($this->moduleTemplate->renderContent());
     }
 
+
+    public function generateAlertMessage(bool $success){
+        if($success){
+            $alertMessageHeader = $this->localizationUtility->translate(SELF::LANG_FILE.'import_success_body');
+            $alertMessageBody =    $this->localizationUtility->translate(SELF::LANG_FILE.'success');
+            $flashMessageService =  AbstractMessage::OK;
+        }
+        else{
+            if($this->duplicatedSourcePath == ''){
+                $alertMessageHeader = $this->localizationUtility->translate(SELF::LANG_FILE.'import_error_syntax');
+                $alertMessageBody =    $this->localizationUtility->translate(SELF::LANG_FILE.'error');
+                $flashMessageService =   AbstractMessage::ERROR;
+            }
+            else{
+                $alertMessageHeader = $this->localizationUtility->translate(SELF::LANG_FILE.'import_error_duplicated')
+                    . ' "'. $this->duplicatedSourcePath .'"'.$this->localizationUtility->translate(SELF::LANG_FILE.'is_duplicated') ;
+                $alertMessageBody =    $this->localizationUtility->translate(SELF::LANG_FILE.'error');
+                $flashMessageService =   AbstractMessage::ERROR;
+            }
+        }
+
+        $message = GeneralUtility::makeInstance(FlashMessage::class,
+            $alertMessageHeader,
+            $alertMessageBody,
+            $flashMessageService,
+        );
+        $flashService = GeneralUtility::makeInstance(FlashMessageService::class);
+        $messageQueue = $flashService->getMessageQueueByIdentifier();
+        $messageQueue->addMessage($message);
+    }
     /**
      * Persist imported list of redirects in DB
      *
      * @return bool TRUE if the was list was successfully stored in the database
      */
-    protected function createRedirectsList(array $redirectListArray): bool
+    protected function importRedirects(array $redirectListArray): bool
     {
         $validImport = true;
-        $importedRows = [];
+        $rows = [];
         // get the source_paths from the DB
         $sourcePathArray = $this->redirectRepository->getSourcePaths();
         // precessing each line in the array
         foreach ($redirectListArray as $item){
             // separate the row columns
-            $row = explode($this->getSeparationChar(),$item);
+            $row = explode($this->separatedChars[$this->selectedSeparatedChar],$item);
             // empty line
             if(count($row) == 1 && $row[0] == ''){
                 continue;
@@ -136,10 +153,10 @@ class AddRedirectsController  extends BackendModuleActionController
             // verify if all columnus are valide
             // we have to make sure that we have all important fields
             // make sure that all the fields are valid
-            if(count($row) == 8){
+            if(count($row) == SELF::NUMBER_OF_ROW){
                 // Mapping item to Demand Redirect Reposiotry
                 // title, source host, source path, target, start time, end time, is regular experssion, status code
-                $redirectItem = new Redirect();
+                /*$redirectItem = new Redirect();
                 $redirectItem->setTitle($row[0]);
                 $redirectItem->setSourceHost($row[1]);
                 $redirectItem->setSourcePath($row[2]);
@@ -151,8 +168,10 @@ class AddRedirectsController  extends BackendModuleActionController
                 $redirectItem->setStartTime((int)$startTime);
                 $redirectItem->setEndTime((int)$endDate);
                 $redirectItem->setIsRegExp((int)$row[6]);
-                $redirectItem->setTargetStatusCode((int)$row[7]);
-                array_push($importedRows, $redirectItem);
+                $redirectItem->setTargetStatusCode((int)$row[7]);*/
+                $row[4] = strtotime($row[4]);
+                $row[5] = strtotime($row[5]);
+                array_push($rows, $row);
                 // verify if the source_path already exists
                 if(in_array($row[2], $sourcePathArray, TRUE)){
                     $validImport = false;
@@ -169,9 +188,10 @@ class AddRedirectsController  extends BackendModuleActionController
 
         // save the items if all import are valid
         if($validImport == true){
-            if(!is_null($importedRows) && count($importedRows) > 0){
-                foreach ($importedRows as $item){
-                    $this->redirectRepository->saveRedirect($item);
+            if(!is_null($rows) && count($rows) > 0){
+                foreach ($rows as $item){
+
+                    //$this->redirectRepository->saveRedirect($item);
                 }
                 $validImport = true;
             }
@@ -183,33 +203,16 @@ class AddRedirectsController  extends BackendModuleActionController
     }
 
 
-    /**
-     *
-     * @return   string      the separation character
-     */
-    private function getSeparationChar(): string
+    protected function importRedirect($rows)
     {
-        $character = $this->request->getParsedBody()['separationCharacter'];
-        switch ($character) {
-            case 'tab':
-                $character = "\t";
-                break;
-            case 'pipe':
-                $character = '|';
-                break;
-            case 'semicolon':
-                $character = ';';
-                break;
-            case 'colon':
-                $character = ':';
-                break;
-            case 'comma':
-            default:
-                $character = ',';
-                break;
-        }
 
-        return $character;
+        $datamap = [];
+        foreach ($rows as $key => $item) {
+            $datamap['sys_redirects']['NEW_'.$key] = $item;
+        }
+        $dataHandler = $this->getDatahandler($datamap);
+        $dataHandler->process_datamap();
+        $success[] = count($datamap[$this->tableName]) . ' événements ont été importés.';
     }
 
     /**
