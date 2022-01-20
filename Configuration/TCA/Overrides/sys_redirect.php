15077015f3999a2d8c86e80082ef5b074b7f961b
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-01-20 15:49:05 +0100

    add title field in Redirections module

---
+++ Configuration/TCA/Overrides/sys_redirect.php
@@ -0,0 +1,23 @@
+<?php
+defined('TYPO3') or die();
+
+// Add some fields to fe_users table to show TCA fields definitions
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_redirect',
+    [
+        'title' => [
+            'exclude' => true,
+            'label' => 'title',
+            'config' => [
+                'type' => 'input',
+                'default' => 0
+            ]
+        ],
+
+    ]
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'sys_redirect',
+    'title',
+    '',
+    'before:source_host'
+);
