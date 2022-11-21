46a8c7a46b9426a6a31fa31836f4916ea6342349
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-11-21 14:43:48 +0100

    modify extension description, add xls file for records example

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,12 +5,12 @@
  */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Qc Redirects',
-    'description' => 'Module used to improve the redirects module, by importing a redirects list with a title column',
+    'description' => "Extends Core's Redirects Module with a Title column, showing creation and modification date and more sorting and filtering options. Also add an import redirects functionality.",
     'author' => 'Quebec.ca',
     'category' => 'module',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.6.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [
         ],
