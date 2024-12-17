338a0708868f4462e318459645354d8abf6e11d9
Abderrazak Tigiti <abderrazak.tigiti@mce.gouv.qc.ca>
2024-12-17 13:16:40 +0100

    Add supported version of TYPO3

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,14 +9,13 @@ $EM_CONF[$_EXTKEY] = [
     'description' => "Extends Core's Redirects Module with a Title column, showing creation and modification date and more sorting and filtering options. Also add an import redirects functionality.",
     'author' => 'Quebec.ca',
     'category' => 'module',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'php' => '8.2',
-            'typo3' => '^12.4',
+            'typo3' => '12.4.0-12.9.99',
         ],
-        'conflicts' => [
-        ],
+        'conflicts' => []
     ],
-    'state' => 'beta',
-    'version' => '2.0.0'
+    'state' => 'beta'
 ];
